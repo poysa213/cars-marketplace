4ec6b94e2c13e22fbc7311700045d7ae3a346f12
poysa213 <y.hanaia@esi-sba.dz>
2023-03-07 13:53:17 +0100

    configure .env

--- core/settings.py
+++ core/settings.py
@@ -15,6 +15,9 @@ from pathlib import Path
 from datetime import timedelta
 import os
 
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#r2a*51v$%c81k%4_2644rt^0z=6z^zc4s-rnnae(u+f&xlj7g'
+SECRET_KEY = os.environ['SECRET_KEY']
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
