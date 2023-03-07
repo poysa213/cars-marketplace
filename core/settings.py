3ed6684c661f4bc4dfffa09595efc57d652b371d
poysa213 <y.hanaia@esi-sba.dz>
2023-03-07 16:47:45 +0100

    collect stati

--- core/settings.py
+++ core/settings.py
@@ -142,6 +142,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Base url to serve media files
 MEDIA_URL = '/media/'
